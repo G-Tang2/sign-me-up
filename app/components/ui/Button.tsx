0440431babb33a6bbf55bfb41cceccedd2935781
Garvin Tang <garvin9006@gmail.com>
2025-07-05 18:41:26 +1000

    added home page

---
+++ app/components/ui/Button.tsx
@@ -0,0 +1,7 @@
+export default function Button({text}: {text: string}) {
+    return (
+        <button className="mt-8 px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors">
+            {text}
+        </button>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
-import Image from "next/image";
+import Button from "./components/ui/Button";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">Welcome to Next.js!</h1>
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <h1 className="text-4xl font-bold">Simple Event Planning Made Easy</h1>
       <p className="mt-4 text-lg">
-        This is a simple Next.js application.
+        Create events in seconds and send a unique link to invite friends. No apps, no fuss — just simple, seamless planning from any device.
       </p>
+      <Link href="/create">
+        <Button text="Create An Event" />
+      </Link>
     </main>
   );
 }
